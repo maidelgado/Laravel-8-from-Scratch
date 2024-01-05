aba4ed955b661a451771e31fb6ab6cf290dd2e12
maidelgado <mdelgado@jlabs.team>
2024-01-05 19:26:37 +0800

    used Eloquent

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,16 +1,15 @@
 <x-layout>
-  @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-              {{ $post->title}}
-            </a>
-        </h1>
-
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-  @endforeach
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
 </x-layout>
-
+ 
