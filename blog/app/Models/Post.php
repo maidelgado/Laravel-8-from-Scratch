4980b264eee06eb2c836f5b2358b1cb8bb6c2a71
maidelgado <mdelgado@jlabs.team>
2024-01-08 10:57:18 +0800

    added eloquent relationship

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
+    protected $guarded = [];
     // protected $fillable = ['title', 'excerpt', 'body'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
 }
