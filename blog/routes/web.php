9d66d9019f00950694a0550b2f95a141aad2049b
maidelgado <mdelgado@jlabs.team>
2024-01-08 12:03:41 +0800

    solved N+1 problem

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -17,7 +21,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
@@ -25,4 +29,10 @@ Route::get('posts/{post:slug}', function (Post $post) { // Post::where('slug', $
     return view('post', [
         'post' => $post
     ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+        ]);
 });
